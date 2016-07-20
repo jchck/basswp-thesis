48786dc71d47e6888300dd3dbf3e9769ba5e43ac
jchck <hey@justinchick.com>
2016-07-20 12:44:48 -0400

    comments

--- logic/assets.php
+++ logic/assets.php
@@ -2,6 +2,10 @@
 
 namespace basswp\thss\assets;
 
+/*
+	Add CSS & JS to the front
+*/
+
 function assets(){
 	wp_enqueue_style( 'css', get_template_directory_uri() . '/dest/thesis.css', false, null );
 
@@ -12,9 +16,19 @@ function assets(){
 	}
 
 	/*
-		Enqueue external stylesheets as such
+		Enqueue external stylesheets as such:
 	*/
 	/* wp_enqueue_style( 'gFont', '//fonts.googleapis.com/css?family=...', false, null ); */
 }
 
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
+
+/*
+	Use main CSS file for visual editor
+*/
+
+function visual_editor(){
+	add_editor_style( get_template_directory_uri() . '/dest/thesis.css' );
+}
+
+add_action( 'after_theme_setup', __NAMESPACE__ . '\\visual_editor' );
