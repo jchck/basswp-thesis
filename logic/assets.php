09c4b5df2138258ff4e1d47ed2e85ab8d642bc07
jchck <hey@justinchick.com>
2016-07-20 12:51:48 -0400

    update commenting syntax

--- logic/assets.php
+++ logic/assets.php
@@ -2,9 +2,12 @@
 
 namespace basswp\thss\assets;
 
-/*
-	Add CSS & JS to the front
-*/
+/**
+ *
+ * Add CSS & JS to the front
+ * [1] To add external CSS to the front (ie: fonts.google.com)
+ *
+ */
 
 function assets(){
 	wp_enqueue_style( 'css', get_template_directory_uri() . '/dest/thesis.css', false, null );
@@ -15,17 +18,16 @@ function assets(){
 		wp_enqueue_script( 'comment-reply' );
 	}
 
-	/*
-		Enqueue external stylesheets as such:
-	*/
-	/* wp_enqueue_style( 'gFont', '//fonts.googleapis.com/css?family=...', false, null ); */
+	// wp_enqueue_style( 'gFont', '//fonts.googleapis.com/css?family=...', false, null ); /* [1] */ 
 }
 
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
 
-/*
-	Use main CSS file for visual editor
-*/
+/**
+ *
+ * Use amin CSS file for visual editor
+ *
+ */
 
 function visual_editor(){
 	add_editor_style( get_template_directory_uri() . '/dest/thesis.css' );
